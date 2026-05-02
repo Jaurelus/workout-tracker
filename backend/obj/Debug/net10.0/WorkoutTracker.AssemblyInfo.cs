716bc6f670c5e50cc578cc0ce6cbbf2372b6f35c
“Jaurelus” <hrqrdbrdzf@privaterelay.appleid.com>
2026-05-01 20:08:33 -0400

    frontend dashboard layout

--- backend/obj/Debug/net10.0/WorkoutTracker.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/WorkoutTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkoutTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51417c09dc6b3654c8613b41cb9e17a171784b1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e10767c5a100c9e09132797eb41f674b928cebba")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkoutTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkoutTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,81 @@
 import { useState } from "react";
 
 import "./App.css";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroupAction,
+  SidebarHeader,
+  SidebarInset,
+  SidebarMenu,
+  SidebarMenuAction,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "./components/ui/sidebar";
+import { AppSidebar } from "./components/app-sidebar";
+import { useSidebar } from "./components/ui/sidebar";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
-    <div className="justify-between flex flex-1 flex-col">
-      <div className="header">
-        <h1>Workout Tracker</h1>
-      </div>
-      <div className="mainContent bg-white">
-        <p>HI</p>
-      </div>
-      <div className="footer"></div>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <SidebarProvider className="text-center">
+        <Sidebar
+          variant="sidebar"
+          className="flex-row flex-1 flex justify-between"
+        >
+          <SidebarHeader className="py-8 font-bold text-xl">Work</SidebarHeader>
+          <SidebarContent className="mt-12">
+            <SidebarMenu className="gap-8">
+              <SidebarMenuItem>
+                <Button variant="ghost">Log a New Workout</Button>
+                <SidebarMenuAction></SidebarMenuAction>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <Button variant="ghost">Add a new exercise </Button>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <Button variant="ghost"> View Past Workouts</Button>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarContent>
+        </Sidebar>
+        <SidebarTrigger></SidebarTrigger>
+
+        <main className="w-screen flex flex-col flex-1 items-center p-5">
+          <h1 className="text-4xl">Workout Tracker</h1>
+          <div className="absolute right-3">
+            <ModeToggle />
+          </div>
+          {/* Dashboard Panels */}
+          <div className="w-full flex-col gap-5 flex items-center  pt-20">
+            {/*Top Row*/}
+            <div className="flex flex-row gap-5 w-full justify-center">
+              <div className="bg-primary-foreground w-[40%] h-80 rounded-3xl p-2">
+                <h1 className="text-2xl text-white">Last Workout</h1>
+              </div>
+
+              <div className="bg-primary-foreground w-[40%] rounded-3xl p-2">
+                <h1 className="text-2xl text-white">Consistency</h1>
+              </div>
+            </div>
+            {/*Bottom Row*/}
+            <div className="flex flex-row gap-5 w-full justify-center">
+              <div className="bg-primary-foreground w-[40%] h-80 rounded-3xl p-2">
+                <h1 className="text-2xl text-white">Chart</h1>
+              </div>
+
+              <div className="bg-primary-foreground w-[40%] rounded-3xl p-2">
+                <h1 className="text-2xl text-white">Motivation</h1>
+              </div>
+            </div>
+          </div>
+        </main>
+      </SidebarProvider>
+    </ThemeProvider>
   );
 }
 
